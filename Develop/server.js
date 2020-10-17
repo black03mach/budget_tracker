6d8940b17c5efb8ba8222a91953b569371ab64d3
Patrick Wells <pat.taylorwells@gmail.com>
2020-10-17 16:21:26 -0400

    local

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -17,8 +17,12 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
 
 // routes
 app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on ${PORT}`)
+})
